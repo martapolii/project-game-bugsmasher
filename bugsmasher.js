24fe8f64e3c3d84be4cdac1137db0dbfdd905eb1
Marta <118316713+martapolii@users.noreply.github.com>
2025-01-23 17:37:37 -0500

    Update to prevent any access to the Inspect Element feature, so no one can cheat the game

--- bugsmasher.js
+++ bugsmasher.js
@@ -125,4 +125,44 @@ speedButton.onclick = function () {
 }
 
 
+/*----------- Disabling Inspect Element ---------*/
+// from: https://andrewstutorials.blogspot.com/2014/03/disable-ways-to-open-inspect-element-in.html
 
+// For Keyboard Disable Js
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
+    return false;
+    }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+  return false;
+  }
+  }
+
+  // For Mouse Inspect Disable Js
+  $(function() {
+    $(this).bind("contextmenu", function(e) {
+    e.preventDefault();
+    });
+    });
+    
+  // For All Browsers:
+  window.oncontextmenu = function () {
+  return false;
+  }
+  $(document).keydown(function (event) {
+  if (event.keyCode == 123) {
+  return false;
+  }
+  else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+  return false;
+  }
+  });
